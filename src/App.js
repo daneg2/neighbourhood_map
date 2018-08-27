43d8af4ecb19060481486490fb7320951273f994
Every One <coder@megacoderbox.keashome>
2018-08-27 09:06:33 +0200

    feat: React install, SASS install, Initial styling

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="header">Header</header>
+        <div className="body-container">
+          <article className="map">
+            <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>  
+          </article>
+          <aside className="aside list">Aside 1</aside>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
