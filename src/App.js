2fbef7620406728d894f9157d699d3a99f119158
daneg2 <dane@g2design.co.za>
2018-08-29 16:18:01 +0200

    feat: Add Geocoder to get lat and long from location name

--- src/App.js
+++ src/App.js
@@ -8,12 +8,32 @@ class App extends Component {
     super(props);
     this.state = {
         active: true,
-        locations: {
-
-        }
+        locations: [
+          {title: 'National Portrait Gallery, Washington, DC, USA'},
+          {title: 'National Mall, Washington, DC, USA'},
+          {title: 'Founding Farmers DC, Pennsylvania Avenue Northwest, Washington, DC, USA'},
+          {title: 'Thomas Jefferson Memorial, East Basin Drive Southwest, Washington, DC, USA'},
+          {title: 'Georgetown Cupcake, M Street Northwest, Washington, DC, USA'},
+          {title: 'Shenanigan’s Irish Pub, 18th Street Northwest, Washington, DC, USA'}
+        ]
     };
   }
 
+  componentDidMount = () => {
+    const locations = this.state.locations.map( location => {
+      var geocoder =  new window.google.maps.Geocoder();
+      geocoder.geocode(
+        { address: location.title }, function(results, status) {
+          if (status == google.maps.GeocoderStatus.OK) {
+            console.log(results)
+          } else {
+            window.alert('No luck finding that location - please try again!');
+          }
+        }
+      )
+    });
+  }
+ 
   toggleClass = () => {
     this.setState({ active: !this.state.active})
   };
